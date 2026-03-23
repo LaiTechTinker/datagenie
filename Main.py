d53d8baab34e52a8f16a8496a991640e958424ee
laiwola <layiibrahim32@gmail.com>
2026-03-23 18:58:08 +0100

    profilling endpoint done

--- Main.py
+++ Main.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import os
+from werkzeug.utils import secure_filename
+from flask import Flask,jsonify,request
+from flask_cors import CORS
 from pathlib import Path
 from Data_insights import generate_insights
 from profiling import profile_data,profile_to_text
@@ -9,7 +13,46 @@ from Cleaningsum import explain_cleaning
 from Mlengine import run_automl
 from Mlexplanation import explain_results
 
-def run_profiling_pipeline(file_path: str):
+# let initiate flask here
+app=Flask(__name__)
+CORS(app)
+# Folder to store uploads
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = {"csv", "xlsx"}
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+@app.route("/",methods=["GET"])
+def send_ui():
+    return jsonify({
+        "message":"hello brother"
+    })
+@app.route("/upload_file",methods=["POST"])
+def run_profiling_pipeline():
+ try:
+    # check if a file is uploaded
+    if "file" not in request.files:
+        return  jsonify({
+            'error':"No files is provided"}),400
+    # this allows only one file to be uploaded at a time
+    if  len(request.files.getlist("file"))!=1:
+        return  jsonify({
+            'error':"Please upload only one file at a time"}),400
+    file=request.files["file"]
+    file_name=file.filename
+    # check is file_name is not empty
+    if file_name=="":
+        return  jsonify({
+            'error':"No file is selected"}),400
+    # Secure filename
+    file_name = secure_filename(file.filename)
+    extension=Path(file_name).suffix.lower()
+   
+    if extension not in [".csv",".xlsx"]:
+        return jsonify({
+            'error':"Invalid file format. Please upload a CSV or Excel file"}),400
+    file_path = os.path.join(app.config["UPLOAD_FOLDER"], file_name)
+    file.save(file_path)
     # Step 1: Profile dataset
     profile = profile_data(file_path)
 
@@ -23,6 +66,8 @@ def run_profiling_pipeline(file_path: str):
         "profile": profile,
         "insights": insights
     }
+ except Exception as e:
+    return jsonify({"error": f"{e}"}),500
 
 # this code block will generate data visualixation
 def run_visualization_pipeline(file_path: str, user_query: str):
@@ -83,3 +128,7 @@ def run_ml_pipeline(file_path: str, target_column: str):
         "result": result,
         "explanation": explanation
     }
+
+# app server starting
+if __name__ =="__main__":
+    app.run(debug=True)
